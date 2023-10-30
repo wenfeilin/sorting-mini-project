3796f87a45f4abe6582ab146d057c0bcc5ecc084
Wenfei Lin <wl688728@gmail.com>
2023-10-30 02:49:48 -0500

    Implement 8 more tests

--- src/SortTester.java
+++ src/SortTester.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test;
 /**
  * Tests of Sorter objects.
  *
- * @author Your Name
+ * @author Wenfei Lin
  */
 public class SortTester {
 
@@ -44,5 +44,69 @@ public class SortTester {
     sorter.sort(original, (x, y) -> x.compareTo(y));
     assertArrayEquals(original, expected);
   } // orderedStringTest
+
+  @Test
+  public void noElementsTest() {
+    String[] original = {};
+    String[] expected = {};
+    sorter.sort(original, (x, y) -> x.compareTo(y));
+    assertArrayEquals(original, expected);
+  } // noElementsTest
+
+  @Test
+  public void oneElementTest() {
+    String[] original = { "alpha" };
+    String[] expected = { "alpha" };
+    sorter.sort(original, (x, y) -> x.compareTo(y));
+    assertArrayEquals(original, expected);
+  } // oneElementTest
+
+  @Test
+  public void twoElementsTest() {
+    String[] original = { "brave", "alpha" };
+    String[] expected = { "alpha", "brave" };
+    sorter.sort(original, (x, y) -> x.compareTo(y));
+    assertArrayEquals(original, expected);
+  } // twoElementsTest
+  
+  @Test
+  public void onlyTwoUnsortedElementsAtEndTest() {
+    String[] original = { "alpha", "bravo", "charlie", "foxtrot", "delta" };
+    String[] expected = { "alpha", "bravo", "charlie", "delta", "foxtrot" };
+    sorter.sort(original, (x, y) -> x.compareTo(y));
+    assertArrayEquals(original, expected);
+  } // onlyTwoUnsortedElementsAtEndTest
+
+  @Test
+  public void onlyTwoUnsortedElementsAtFrontTest() {
+    String[] original = { "bravo", "alpha", "charlie", "delta", "foxtrot" };
+    String[] expected = { "alpha", "bravo", "charlie", "delta", "foxtrot" };
+    sorter.sort(original, (x, y) -> x.compareTo(y));
+    assertArrayEquals(original, expected);
+  } // onlyTwoUnsortedElementsAtFrontTest
+
+  @Test
+  public void onlyTwoUnsortedElementsAtOppositeEndsTest() {
+    String[] original = { "foxtrot", "bravo", "charlie", "delta", "alpha" };
+    String[] expected = { "alpha", "bravo", "charlie", "delta", "foxtrot" };
+    sorter.sort(original, (x, y) -> x.compareTo(y));
+    assertArrayEquals(original, expected);
+  } // onlyTwoUnsortedElementsAtOppositeEndsTest
+
+  @Test
+  public void twoRepeatedElementsTest() {
+    String[] original = { "alpha", "bravo", "charlie", "alpha", "foxtrot" };
+    String[] expected = { "alpha", "alpha", "bravo", "charlie", "foxtrot" };
+    sorter.sort(original, (x, y) -> x.compareTo(y));
+    assertArrayEquals(original, expected);
+  } // twoRepeatedElementsTest
+
+  @Test
+  public void halvesSortedTest() {
+    String[] original = { "delta", "foxtrot", "alpha", "bravo", "charlie" };
+    String[] expected = { "alpha", "bravo", "charlie", "delta", "foxtrot" };
+    sorter.sort(original, (x, y) -> x.compareTo(y));
+    assertArrayEquals(original, expected);
+  } // halvesSortedTest
 
 } // class SortTester
