cc94b331c7cd90bd97ea887953abe1f60c469d66
Wenfei Lin <wl688728@gmail.com>
2023-12-15 05:33:10 -0600

    Fix style, comments, spacing in LinWenfeiSort.java and update CHANGE.md

---
+++ CHANGE.md
@@ -0,0 +1,17 @@
+Changes in README.md:
+- deleted all the `<br>`, replacing them with blank lines, in the file
+
+Changes in LinWenfeiSort.java:
+- changed certain lines that had 4-space indentation to the proper 2-space indentation style
+- wrote about the prompts I asked ChatGPT in the class Javadoc
+
+Changes in InsertionSort.java:
+- created an insert helper method
+
+Changes in InsertionSort.java, LinWenfeiSort.java, MergeSort.java, Quicksort.java:
+- added proper comments to the right braces 
+- moved all comments on the side of the code before the code
+- edited each line to have 80 or fewer characters
+
+Other Changes:
+- Created Utils.java file with a static swap method to be referenced in InsertionSort.java and Quicksort.java to avoid repetition of code
--- src/LinWenfeiSort.java
+++ src/LinWenfeiSort.java
@@ -1,7 +1,14 @@
+import java.util.Arrays;
 import java.util.Comparator;
 
 /**
  * Sort using a modified version of TimSort generated from ChatGPT.
+ * Some prompts I asked ChatGPT:
+ * > Please write a generic TimSort algorithm with Google 
+ *   Java Style and two-space indentation in Java.
+ * > with generics
+ * > please add comments to the code too!
+ * > how could you optimize the insertionSort in TimSort?
  *
  * @author ChatGPT, Wenfei Lin
  */
@@ -38,84 +45,87 @@ public class LinWenfeiSort implements Sorter {
   // +---------+
 
   /**
-   * Sort an array using a somewhat modified TimSort (mergeSort + insertionSort).
+   * Sort an array using a somewhat modified TimSort (mergeSort +
+   * insertionSort).
    * 
    * @param values The array being sorted.
-   * @param order The comparator that determines what order the elements in the array
-   *              will be sorted in.
+   * @param order  The comparator that determines what order the elements in
+   *               the array will be sorted in.
    * @post The array will be sorted in the order delineated by the comparator.
    */
   @Override
   public <T> void sort(T[] values, Comparator<? super T> order) {
-    int arrLength = values.length;
+    int arrLen = values.length;
 
-    // Sort the small-sized chunks (determined by MIN_MERGE) of the array using 
-    // insertion sort:
-    for (int i = 0; i < arrLength; i += MIN_MERGE) {
-        insertionSort(values, order, i, Math.min((i + MIN_MERGE - 1), (arrLength - 1)));
-    }
+    // Sort the small-sized chunks (determined by MIN_MERGE)
+    // of the array using insertion sort:
+    for (int i = 0; i < arrLen; i += MIN_MERGE) {
+      insertionSort(values, order, i,
+          Math.min((i + MIN_MERGE - 1), (arrLen - 1)));
+    } // for
 
     // Merge sorted chunks (using the merge from mergeSort)... keep merging
     // until whole array is merged back together
-    for (int size = MIN_MERGE; size < arrLength; size = size * 2) {
-        for (int left = 0; left < arrLength; left += 2 * size) {
-            int mid = Math.min(arrLength, left + size);
-            int right = Math.min(arrLength, left + 2 * size);
-            
-            // Check if the two chunks are already sorted
-            if (order.compare(values[mid - 1], values[mid]) <= 0) {
-                continue;  // Skip the merge if not needed
-            }
+    for (int size = MIN_MERGE; size < arrLen; size = size * 2) {
+      for (int left = 0; left < arrLen; left += 2 * size) {
+        int mid = Math.min(arrLen, left + size);
+        int right = Math.min(arrLen, left + 2 * size);
 
-            merge(values, order, left, mid, right);
-        }
-    }
+        // Check if the two chunks are already sorted
+        if (order.compare(values[mid - 1], values[mid]) <= 0) {
+          // Skip the merge if not needed
+          continue;
+        } // if
+
+        merge(values, order, left, mid, right);
+      } // for
+    } // for
   } // sort(T[], Comparator<? super T>)
 
   /**
    * Sort an array using the insertion sort algorithm.
    * 
    * @param values The array being sorted.
-   * @param order The comparator that determines what order the elements in the array 
-   *              will be sorted in.
-   * @param left The leftmost index of the array 
-   * @param right The rightmost index of the array
+   * @param order  The comparator that determines what order the elements
+   *               in the array will be sorted in.
+   * @param left   The leftmost index of the array
+   * @param right  The rightmost index of the array
    * @post The array will be sorted in the order delineated by the comparator.
    */
-  private <T> void insertionSort(T[] values, Comparator<? super T> order, int left, int right) {
+  private <T> void insertionSort(T[] values, Comparator<? super T> order,
+      int left, int right) {
     for (int i = left + 1; i <= right; i++) {
-      T key = values[i];  // Reuse a single key variable
+      // Reuse a single key variable
+      T key = values[i];
       int j = i - 1;
-      
-      // Store the comparison result in a variable for efficiency
-      int comparisonResult;
-      
-      while (j >= left && (comparisonResult = order.compare(values[j], key)) > 0) {
-          values[j + 1] = values[j];
-          j--;
-      }
-      
+
+      while (j >= left && order.compare(values[j], key) > 0) {
+        values[j + 1] = values[j];
+        j--;
+      } // while
+
       values[j + 1] = key;
-    }
+    } // for
   } // insertionSort(T[], Comparator<? super T>, int, int)
 
   /**
    * Sort with the algorithm of merge from mergeSort (modified for TimSort).
    * 
    * @param values The array being sorted.
-   * @param order The comparator that determines what order the elements in the array 
-   *              will be sorted in.
-   * @param left The leftmost index of the subarray being considered
-   * @param mid The middle index of the subarray being considered
-   * @param right The rightmost index of the subarray being considered
-   * @post The subarray with its boundaries defined by left and right in the original 
-   *       array is sorted.
+   * @param order  The comparator that determines what order the elements
+   *               in the array will be sorted in.
+   * @param left   The leftmost index of the subarray being considered
+   * @param mid    The middle index of the subarray being considered
+   * @param right  The rightmost index of the subarray being considered
+   * @post The subarray with its boundaries defined by left and right in the
+   *       original array is sorted.
    */
   private <T> void merge(T[] values, Comparator<? super T> order, int left, int mid, int right) {
-    // Calculate the sizes of the left and right subarrays
+    System.out.println("Merge called with left=" + left + ", mid=" + mid + ", right=" + right);
+
     int leftSize = mid - left;
     int rightSize = right - mid;
-    
+
     // Create an array to store the merged elements
     Object[] merged = new Object[leftSize + rightSize];
 
@@ -123,25 +133,25 @@ public class LinWenfeiSort implements Sorter {
     while (i < leftSize && j < rightSize) {
       // Compare elements from the left and right subarrays
       if (order.compare(values[left + i], values[mid + j]) <= 0) {
-        // If the element from the left subarray is smaller or equal, 
+        // If the element from the left subarray is smaller or equal,
         // copy it to the merged array
         merged[k++] = values[left + i++];
       } else {
-        // If the element from the right subarray is smaller, 
+        // If the element from the right subarray is smaller,
         // copy it to the merged array
         merged[k++] = values[mid + j++];
-      }
-    }
+      } // if/else
+    } // while
 
     // Copy any remaining elements from the left subarray to the merged array
     while (i < leftSize) {
       merged[k++] = values[left + i++];
-    }
+    } // while
 
     // Copy any remaining elements from the right subarray to the merged array
     while (j < rightSize) {
       merged[k++] = values[mid + j++];
-    }
+    } // while
 
     // Copy the merged elements back to the original array
     System.arraycopy(merged, 0, values, left, leftSize + rightSize);
